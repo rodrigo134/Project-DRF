c9e940a9eda3346a0427682bbc6ba0c7879f25b9
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2025-04-06 16:16:31 -0300

    remove some things

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 
 from django.urls import path
-from recipes.views import home,contato,my_view
+from recipes.views import my_view
 
 
 urlpatterns = [
     path('', my_view),
-    path('home/',home),
-    path('contato/',contato),
 ]
