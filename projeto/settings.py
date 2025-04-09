984fc8f0859a60e09bb736c6a4afb359ea40355e
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2025-04-09 15:11:16 -0300

    settings in css structure

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # I added the recipes app
     'recipes',
 ]
 
@@ -55,7 +56,8 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'base_templates',
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
