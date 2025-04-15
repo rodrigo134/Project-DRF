a915e731f3488e94712098aa2881e76938a85e21
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2025-04-15 19:55:34 -0300

    Add is_detail_page variable to hide see more button on recipe detail page

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 # Create your views here.
  
 
 def home(request):
-    return render(request,'pages/home.html')
+    return render(request,'pages/home.html', context={
+        'recipes':[make_recipe () for _ in range (10)]
+    })
 
 
 def recipes(request,id):
-    return render(request,'pages/recipe-view.html', context={'name':'rodrigo'})
+    return render(request,'pages/recipe-view.html', context={
+        'recipe':make_recipe(),
+        'is_detail_page': True,
+        })
