6f27c570c608820228259dd5ee2e368cf913e160
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2025-05-14 10:09:34 -0300

    pages not found

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+
 # Create your views here.
  
 
@@ -9,17 +10,27 @@ def home(request):
     recipes = Recipe.objects.filter(is_publish = True).order_by('-id')
     return render(request,'pages/home.html', context={
         
-        'recipes':recipes
+        'recipes':recipes,
+        
     })
 
 
 
 
 def category(request,category_id):
-    recipes = Recipe.objects.filter(category__id=category_id,is_publish = True).order_by('-id')
-    return render(request,'pages/home.html', context={
-        
-        'recipes':recipes
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_publish = True
+        ).order_by('-id')
+    ''' 
+    category_name = getattr(getattr(recipes.first(), 'category', None),
+                    'name','Not found')
+    '''
+    if not recipes:
+        raise Http404('Not Found 😘')
+    return render(request,'pages/category.html', context={
+        'recipes':recipes,
+        'title':  f'{recipes.first().category.name} - Category | ' ,
     })
 
 
